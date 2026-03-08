30157af48e61493935c32258f7ece35cfbde31ad
rejathjohny <rejathjohny7@gmail.com>
2026-03-08 13:41:49 +0530

    Update age print statement to include '/online'

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def main():
     print("Hello, world!")
     print("Rejath Johny")
     print("Name: Rejath Johny")
-    print("Age: 2000")
+    print("Age: 2000/online")
 
 if __name__ == "__main__":
     main()
