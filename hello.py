2b0c244daf866a2a668be60ca3420e20757e2684
Rejath Johny <rejathjohny7@gmail.com>
2026-03-08 13:42:30 +0530

    /local

--- hello.py
+++ hello.py
@@ -2,7 +2,7 @@ def main():
     print("Hello, world!")
     print("Rejath Johny")
     print("Name: Rejath Johny")
-    print("Age: 2000/online")
+    print("Age: 2000/online/local")
 
 if __name__ == "__main__":
     main()
