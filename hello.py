79e5c7c53e79da5efdaa38d320bc9bf4932856f8
Rejath Johny <rejathjohny7@gmail.com>
2026-03-08 11:11:25 +0530

    1994 only commit

--- hello.py
+++ hello.py
@@ -2,7 +2,8 @@ def main():
     print("Hello, world!")
     print("Rejath Johny")
     print("Name: Rejath Johny")
-    print("Age: 1992")
+    print("Age: 1994")
+    print("City: Kochi")
 
 if __name__ == "__main__":
     main()
